14126f411fd189fec5627b7ac25bbae8c0366cfb
tuzmusic <tuzmusic@gmail.com>
2018-05-22 11:22:20 -0400

    Deleted all animation references in PlayingCardView

--- FreeCell2/PlayingCardView.swift
+++ FreeCell2/PlayingCardView.swift
@@ -12,65 +12,9 @@ class PlayingCardView: CardView {
 	
 	var cardWidth: CGFloat { return superview!.bounds.width / 11.5 }
 	var cardHeight: CGFloat { return cardWidth * (3.5/2.5) }
-	static var animDelay = 0.0
-	static var animScale: CGFloat = 1.1
-	static var animDuration: Double = 0.1
-	
+
 	var isSelected = false {
 		didSet {
-			let scale = PlayingCardView.animScale
-			let duration = PlayingCardView.animDuration
-			let delay = PlayingCardView.animDelay
-			func scaleAndWiggle () {
-				let rotation = CGFloat(Double.pi / 12)
-				UIView.animate(withDuration: duration, delay: delay, options: .curveEaseInOut,
-				               animations: {
-								self.transform = CGAffineTransform.identity.rotated(by: rotation)
-								self.transform = self.transform.scaledBy(x:scale, y:scale)
-				}) { (_) in
-					UIView.animate(withDuration: duration * 0.9, delay: 0, options: .curveEaseInOut,
-					               animations: {
-									self.transform = CGAffineTransform.identity.rotated(by: -rotation)
-									self.transform = self.transform.scaledBy(x:scale*1.1, y:scale*1.1)
-					})
-					{ (_) in
-						UIView.animate(withDuration: duration * 1, delay: 0, options: .curveEaseInOut,
-						               animations: {
-										self.transform = CGAffineTransform.identity
-						})
-					}
-				}
-			}
-			func scaleAnimation () {
-				UIView.animate(withDuration: duration, delay: 0, options: .curveEaseInOut,
-				               animations: {
-								self.transform = self.transform.scaledBy(x:scale, y:scale)
-				}) { (_) in
-					UIView.animate(withDuration: duration, delay: 0, options: .curveEaseInOut, animations: {
-						self.transform = CGAffineTransform.identity
-					}, completion: {
-						(_) in self.backgroundColor = self.isSelected ? UIColor.lightGray : UIColor.white
-					})
-				}
-			}
-			func wiggleAnimation () {
-				let rotation = CGFloat(Double.pi / 10)
-				UIView.animate(withDuration: duration, delay: 0, options: .curveEaseInOut,
-				               animations: {
-								self.transform = CGAffineTransform.identity.rotated(by: rotation)
-				}) { (_) in
-					UIView.animate(withDuration: duration * 0.9, delay: 0, options: .curveLinear,
-					               animations: {
-									self.transform = CGAffineTransform.identity.rotated(by: -rotation)
-					}, completion: { (_) in
-						UIView.animate(withDuration: duration * 0.9, delay: 0, options: .curveLinear,
-						               animations: {
-										self.transform = CGAffineTransform.identity
-						})
-					})
-				}
-			}
-			// scaleAndWiggle()
 			self.backgroundColor = self.isSelected ? UIColor.lightGray : UIColor.white
 		}
 	}
